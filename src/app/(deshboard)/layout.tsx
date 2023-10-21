1ba1f02149c1fd49975fc4cec44e5fc61d61fe12
masumali23020 <mdmasum0218@.com>
2023-10-21 23:28:30 +0600

    complet nav componet

--- src/app/(deshboard)/layout.tsx
+++ src/app/(deshboard)/layout.tsx
@@ -1,17 +1,17 @@
 "use client";
 import Logo from "@/components/Logo";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
-import UserButton from "@/components/UserButton";
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2 bg-slate-900">
         <Logo />
         <div className="flex gap-4 items-center">
           <ThemeSwitcher />
-          <UserButton />
+          <UserButton afterSignOutUrl="/sign-in" />
         </div>
       </nav>
     </div>
