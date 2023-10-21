c77b72e752754c5342772b9e94466c9a0efd6d30
masumali23020 <mdmasum0218@.com>
2023-10-21 22:48:15 +0600

    deshboread nav ligo

--- src/app/(deshboard)/layout.tsx
+++ src/app/(deshboard)/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Logo from "@/components/Logo";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
 import UserButton from "@/components/UserButton";
@@ -6,9 +7,9 @@ import React from "react";
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <nav>
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
         <Logo />
-        <div>
+        <div className="flex gap-4 items-center">
           <ThemeSwitcher />
           <UserButton />
         </div>
